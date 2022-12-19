514a48c0a1a5e7f0f5330bb3b85b41fe674bacb3
Vincent Shadbolt <vincentshadbolt@Vincents2015MBP.local.lan>
2022-12-18 17:21:05 -0800

    Reorganization and util cleanup

--- gamepool/gamepool.py
+++ gamepool/gamepool.py
@@ -1,5 +1,7 @@
 from redbot.core import commands
 from .utils import Utils
+from datetime import datetime
+import random
 
 class GamePool(commands.Cog):
     """Allows users to add games they'd like to play to a channel pool and then chooses a random game to play"""
@@ -15,35 +17,155 @@ class GamePool(commands.Cog):
     @gamepool.command()
     async def add(self, ctx, *, game):
         """Add a game to the channel's Game Pool"""
-        # Get the game info from steam
-        await Utils.addToPool(ctx, game)
+        # Check if wanted game can be found on steam and write to pool
+        embed = await Utils.gameInfo(game)
+        if embed:
+            await ctx.send("Added \"" + game + "\" to the pool")
+            with open("GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt","a+") as f:
+                f.write(game + "\n")
+        else:
+            await ctx.send("There was an issue finding your game on Steam. Ensure the name is spelled correctly.")
     
     @gamepool.command()
     async def remove(self, ctx, *, game):
         """Remove a game from the channel's Game Pool"""
-        # Delete game from pool
-        await Utils.removeFromPool(ctx, game)
+        # Ensure pool file exists
+        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt"
+        await Utils.makeFile(file)
+        # Rewrite pool excluding specified game
+        found_game = False
+        with open(file, "r+") as f:
+            lines = f.readlines()
+        with open(file, "w+") as f:
+            for line in lines:
+                if line.strip("\n").lower() != game.lower():
+                    f.write(line)
+                else:
+                    found_game = True
+        # Check if game wasn't found in pool
+        if not found_game:
+            await ctx.send("Game was not found in the pool.")
+        else:
+            await ctx.send("Removed \"" + game + "\" from the pool")
     
     @gamepool.command()
-    async def list(self, ctx):
+    async def list(self, ctx, list = True):
         """List the channel's current Game Pool"""
-        # Delete game from pool
-        await Utils.readPool(ctx)
+        game_pool = []
+        # Ensure pool(s) file exists
+        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt"
+        await Utils.makeFile(file)
+        # Read from pool and deduplicate games
+        with open(file,"r+") as f:
+            for line in f:
+                if line not in game_pool:
+                    game_pool.append(line.strip("\n"))
+        # Check if there's nothing in the pool
+        if not game_pool:
+            await ctx.send("No games in the pool!")
+            return False
+        else: 
+            if list:
+                # List off the pool and return
+                message_string = "Here's the current pool: \n"
+                for game in game_pool:
+                    message_string += str("\t- " + game + "\n")
+                await ctx.send(message_string)
+            return game_pool
     
     @gamepool.command()
     async def pick(self, ctx):
         """Pick a game from the channel's Game Pool"""
-        # Get the game info from steam
-        await Utils.pickFromPool(ctx)
+        # Get possible choices in the pool
+        game_pool = await self.list(ctx, False)
+        if game_pool:
+            # Pick random game from the pool
+            game = random.choice(game_pool)
+            # Pick a random host from the channel
+            users = ctx.channel.members
+            host = await Utils.pickHost(users)
+            # Get the game info from steam
+            embed = await Utils.gameInfo(game)
+            # Send result to channel
+            await ctx.send("I pick \"" + game + f"\" and I think {host.mention} should host! I've also removed it from the pool so it doesn't get picked again. \nHere's the link:", embed=embed)
+            # Remove game from the pool
+            file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt"
+            # Rewrite pool excluding specified game
+            with open(file, "r+") as f:
+                lines = f.readlines()
+            with open(file, "w+") as f:
+                for line in lines:
+                    if line.strip("\n").lower() != game.lower():
+                        f.write(line)
+            # Add game and host to the winners pool
+            with open("GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + "_Winners.txt","a+") as f:
+                f.write("\"" + game + "\" on " + datetime.today().strftime('%Y-%m-%d') + " | Hosted by: " + str(host) +"\n")
     
     @gamepool.command()
     async def winners(self, ctx):
         """List the channel's previous Game Pool winners"""
-        # Delete game from pool
-        await Utils.readPool(ctx, True)
+        winner_pool = []
+        # Ensure pool(s) file exists
+        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + "_Winners.txt"
+        await Utils.makeFile(file)
+        # Read from pool into memory
+        with open(file,"r+") as f:
+            for line in f:
+                winner_pool.append(line.strip("\n"))
+        # Check if there's nothing in the pool
+        if not winner_pool:
+            await ctx.send("No past winners.")
+        else:
+            # List off the pool and return
+            message_string = "Here are the past winners: \n"
+            for winner in winner_pool:
+                message_string += str("\t- " + winner + "\n")
+            await ctx.send(message_string)
     
     @gamepool.command()
     async def host(self, ctx, *, game):
         """Pick a new host for the winning game"""
-        # Delete game from pool
-        await Utils.pickNewHost(ctx, game, True)
+        # Ensure pool(s) file exists
+        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + "_Winners.txt"
+        await Utils.makeFile(file)
+        # Check if no game specififed
+        if not game:
+            await ctx.send("No game specified.")
+            return
+        filtered_members = []
+        users = ctx.channel.members
+        # Rewrite pool and attempt to change game host
+        found_game = False
+        with open(file, "r+") as f:
+            lines = f.readlines()
+        with open(file, "w+") as f:
+            for line in lines:
+                if game.lower() in line.lower():
+                    new_line = line.split("Hosted by: ")
+                    old_host = ctx.guild.get_member_named(new_line[1].strip("\n"))
+                    for user in users:
+                        if user != old_host and user.bot != True:
+                            filtered_members.append(user)
+                    try:
+                        new_host = random.choice(filtered_members)
+                        f.write(new_line[0] + "Hosted by: " + str(new_host) + "\n")
+                    except: 
+                        new_host = ""
+                        f.write(new_line[0] + "Hosted by: " + str(old_host) + "\n")
+                    found_game = True
+                else:
+                    f.write(line)
+            # Report to channel
+            if not found_game:
+                await ctx.send("Game was not found in the pool.")
+                return
+            elif new_host != "":
+                try:
+                    await ctx.send(f"{old_host.mention} couldn't cut it huh? Let's make your new host {new_host.mention}")
+                except:
+                    await ctx.send(str(old_host) + " couldn't cut it huh? Let's make your new host " + str(new_host))
+            else:
+                try:
+                    await ctx.send(f"There's no other hosts to choose from! You're all we got {old_host.mention}")
+                except:
+                    await ctx.send("There's no other hosts to choose from! You're all we got " + str(old_host))
--- gamepool/utils.py
+++ gamepool/utils.py
@@ -2,10 +2,9 @@ import aiohttp
 import discord
 import random
 import os
-from datetime import datetime
 
 class Utils:
-    async def gameToId(gamename):
+    async def gameToId(game):
         """Convert a game name to its ID"""
         # Start http session and get Steam game list
         session = aiohttp.ClientSession()
@@ -15,7 +14,7 @@ class Utils:
         response = response['applist']['apps']
         # Parse list of apps for specified game. If not found return
         try:
-            gameid = next((item for item in response if item["name"].lower().replace(" ", "").replace(":", "").replace("!", "") == gamename.lower().replace(" ", "").replace(":", "").replace("!", "")))
+            gameid = next((item for item in response if item["name"].lower().replace(" ", "").replace(":", "").replace("!", "") == game.lower().replace(" ", "").replace(":", "").replace("!", "")))
         except StopIteration:
             await session.close()
             return False
@@ -24,166 +23,29 @@ class Utils:
         await session.close()
         return gameid
 
-    async def gameInfo(ctx, gamename, pickCommand):
+    async def gameInfo(game):
         """Convert game ID to steam link"""
         # Search Steam for game ID
-        appid = await Utils.gameToId(gamename)
+        appid = await Utils.gameToId(game)
         # Check if no games matched the search
         if not appid:
-            await ctx.send("There was an issue finding your game on Steam. Ensure the name is spelled correctly.")
             return False
-        # Message sucessful tag to channel if game was added
-        if not pickCommand:
-            await ctx.send("Added \"" + gamename + "\" to the pool")
-        else:
-            # Message link to Steam game to channel if game was picked
-            embed = discord.Embed(title="Steam Game Information", url="https://store.steampowered.com/app/" + str(appid),
-                                description="Steam Link for " + gamename, color=0x42a6cc)
-            embed.add_field(name="Steam Game ID", value=appid, inline=True)
-            embed.add_field(name="Steam Game Name", value=gamename, inline=True)
-            await ctx.send(embed=embed)
-        return True
-     
-    async def pickNewHost(ctx, game, reroll = False):
-        """Edit the host of winning game"""
-        # Ensure pool(s) file exists
-        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + "_Winners.txt"
-        await Utils.makeFile(file)
-        # Get possible choices in the channel
+        # Message link to Steam game to channel if game was picked
+        embed = discord.Embed(title="Steam Game Information", url="https://store.steampowered.com/app/" + str(appid),
+                                description="Steam Link for " + game, color=0x42a6cc)
+        embed.add_field(name="Steam Game ID", value=appid, inline=True)
+        embed.add_field(name="Steam Game Name", value=game, inline=True)
+        return embed
+
+    async def pickHost(users):
+        """Pick the host of winning game"""
         members = []
-        users = ctx.channel.members
-        if not reroll:
-            for user in users:
-                if user.bot != True:
-                    members.append(user)
-            return random.choice(members)
-        else:
-            # Check if no game specififed
-            if not game:
-                await ctx.send("No game specified.")
-                return
-            filtered_members = []
-            # Rewrite pool and change host game
-            found_game = False
-            with open(file, "r+") as f:
-                lines = f.readlines()
-            with open(file, "w+") as f:
-                for line in lines:
-                    if game.lower() in line.lower():
-                        new_line = line.split("Hosted by: ")
-                        old_host = ctx.guild.get_member_named(new_line[1].strip("\n"))
-                        for user in users:
-                            if user != old_host and user.bot != True:
-                                filtered_members.append(user)
-                        new_host = random.choice(filtered_members)
-                        f.write(new_line[0] + "Hosted by: " + str(new_host) + "\n")
-                        found_game = True
-                    else:
-                        f.write(line)
-                # Check if game wasn't found in pool
-                if not found_game:
-                    await ctx.send("Game was not found in the pool.")
-                    return
-                if old_host is not None and new_host is not None:
-                    await ctx.send(f"{old_host.mention} couldn't cut it huh?")
-                    await ctx.send(f"Let's make your new host {new_host.mention}")
-                    return
-                else:
-                    await ctx.send("Old host couldn't cut it huh?")
-                    await ctx.send("Let's make your new host @" + str(new_host))
-    
+        for user in users:
+            if user.bot != True:
+                members.append(user)
+        return random.choice(members)
+
     async def makeFile(file):
+        """Make a new text file"""
         if not os.path.exists(file):
             f = open(file, "w+")
-    
-    async def removeFromPool(ctx, game, pickCommand = False):
-        """Get Game Info from Steam"""
-        # Check if no game specififed
-        if not game:
-            await ctx.send("No game specified.")
-            return
-        # Ensure pool file exists
-        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt"
-        await Utils.makeFile(file)
-        # Rewrite pool excluding specified game
-        found_game = False
-        with open(file, "r+") as f:
-            lines = f.readlines()
-        with open(file, "w+") as f:
-            for line in lines:
-                if line.strip("\n").lower() != game.lower():
-                    f.write(line)
-                else:
-                    found_game = True
-        # Check if game wasn't found in pool
-        if not found_game:
-            await ctx.send("Game was not found in the pool.")
-        else:
-            if not pickCommand:
-                await ctx.send("Removed \"" + game + "\" from the pool")
-            else:
-                await ctx.send("I've also removed it from the pool so it doesn't get picked again.")
-    
-    async def addToPool(ctx, game, pickCommand = False, host = ""):
-        """Add a verified steam game to the pool"""
-        # Check if no game specififed
-        if not game:
-            await ctx.send("No game specified.")
-            return
-        # Check if wanted game can be found on steam and write to pool
-        if not pickCommand:
-            valid = await Utils.gameInfo(ctx, game, False)
-            if valid:
-                with open("GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt","a+") as f:
-                    f.write(game + "\n")
-                # await Utils.readPool(ctx)
-        else:
-            with open("GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + "_Winners.txt","a+") as f:
-                    f.write("\"" + game + "\" on " + datetime.today().strftime('%Y-%m-%d') + " | Hosted by: " + str(host) +"\n")
-    
-    async def readPool(ctx, pickCommand = False, justList = False):
-        """Read all games in the pool"""
-        game_pool = []
-        # Ensure pool(s) file exists
-        file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + ".txt"
-        if pickCommand:
-            file = "GamePool_" + str(ctx.guild.id) + "_" + str(ctx.channel.id) + "_Winners.txt"
-        await Utils.makeFile(file)
-        # Read from pool and deduplicate games
-        with open(file,"r+") as f:
-            for line in f:
-                if line not in game_pool:
-                    game_pool.append(line.strip("\n"))
-        # Check if there's nothing in the pool
-        if not game_pool:
-            await ctx.send("No games in the pool!")
-            return False
-        if not justList:
-            # List off the pool and return
-            if pickCommand:
-                message_string = "Here are the past winners: \n"
-            else:
-                message_string = "Here's the current pool: \n"
-            for game in game_pool:
-                message_string += str("\t- " + game + "\n")
-            await ctx.send(message_string)
-        return game_pool
-    
-    async def pickFromPool(ctx, pickCommand = True):
-        """Pick a random game form the pool"""
-        # Get possible choices in the pool
-        game_pool = await Utils.readPool(ctx, False, True)
-        if game_pool:
-            # Pick random game from the pool and send result to channel
-            random_game = random.choice(game_pool)
-            await ctx.send("I pick \"" + random_game + "\", here's the link:")
-            # Get the game info from steam
-            await Utils.gameInfo(ctx, random_game, pickCommand)
-            # Remove it from the pool
-            await Utils.removeFromPool(ctx, random_game, pickCommand)
-            # Pick random host
-            host = await Utils.pickNewHost(ctx, random_game)
-            await ctx.send(f"Oh, and I think {host.mention} should host!")
-            # Add it to the winners pool
-            await Utils.addToPool(ctx, random_game, pickCommand, host)
-   
