c6fb44e83dca35dcb2de9e35d54906e2b3182022
Vincent Shadbolt <vince.shadbolt@gmail.com>
2024-04-16 15:47:21 -0700

    fixed game name in embed links

--- gamepool/utils.py
+++ gamepool/utils.py
@@ -14,27 +14,30 @@ class Utils:
         response = response['applist']['apps']
         # Parse list of apps for specified game. If not found return
         try:
-            gameid = next((item for item in response if item["name"].lower().replace(" ", "").replace(":", "").replace("!", "").replace("™", "") == game.lower().replace(" ", "").replace(":", "").replace("!", "").replace("™", "")))
+            gameData = next((item for item in response if item["name"].lower().replace(" ", "").replace(":", "").replace("!", "").replace("™", "") == game.lower().replace(" ", "").replace(":", "").replace("!", "").replace("™", "")))
         except StopIteration:
             await session.close()
             return False
         # Return game ID and close http session
-        gameid = gameid['appid']
+        gameid = gameData['appid']
+        gamename = gameData['name']
         await session.close()
-        return gameid
+        return gameid, gamename
 
     async def gameInfo(game):
         """Convert game ID to steam link"""
         # Search Steam for game ID
-        appid = await Utils.gameToId(game)
+        appData = await Utils.gameToId(game)
+        appid = appData[0]
+        appName = appData[1]
         # Check if no games matched the search
         if not appid:
             return False
         # Message link to Steam game to channel if game was picked
         embed = discord.Embed(title="Steam Game Information", url="https://store.steampowered.com/app/" + str(appid),
-                                description="Steam Link for " + game, color=0x42a6cc)
+                                description="Steam Link for " + appName, color=0x42a6cc)
         embed.add_field(name="Steam Game ID", value=appid, inline=True)
-        embed.add_field(name="Steam Game Name", value=game, inline=True)
+        embed.add_field(name="Steam Game Name", value=appName, inline=True)
         return embed
 
     async def pickHost(users):
